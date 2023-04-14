77a5777df2671f752ac7258f33c2f96327d7b63d
Doug Knight <doug.knight@priceline.com>
2023-04-14 10:01:58 -0500

    add transaction spec

--- src/xtdb_money/models/transactions.clj
+++ src/xtdb_money/models/transactions.clj
@@ -1,9 +1,20 @@
 (ns xtdb-money.models.transactions
-  (:require [xtdb-money.core :as mny]
+  (:require [clojure.spec.alpha :as s]
+            [xtdb-money.core :as mny]
             [xtdb-money.models.accounts :as acts]))
+
+(s/def ::debit-account-id uuid?)
+(s/def ::credit-account-id uuid?)
+(s/def ::amount (s/and decimal?
+                       #(< 0M %)))
+(s/def ::transaction (s/keys :req-un [::credit-account-id
+                                      ::debit-account-id
+                                      ::amount]))
 
 (defn put
   [{:keys [debit-account-id credit-account-id amount] :as trx}]
+  {:pre [(s/valid? ::transaction trx)]}
+
   (let [d-account (acts/find debit-account-id)
         c-account (acts/find credit-account-id)]
     (mny/put (vary-meta trx assoc :model-type :transaction)
