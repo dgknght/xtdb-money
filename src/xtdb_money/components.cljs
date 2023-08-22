4e945cef82c86967af86d6d9557440c4bc5ad753
Doug Knight <dougbknight@gmail.com>
2023-08-22 18:54:30 -0500

    make title-bar a component

--- src/xtdb_money/components.cljs
+++ src/xtdb_money/components.cljs
@@ -47,43 +47,44 @@
         doall)])
 
 (defn title-bar []
-  [:header.p-3.mb-3.border-bottom
-   [:div.container
-    [:div.d-flex.flex-wrap.align-items-center.justify-content-center.justify-content-lg-start
-     [:a.d-flex.align-items-center.mb-2.mb-lg-0.link-body-emphasis.text-decoration-none.me-2
-      {:href "/"}
-      (icon :cash-coin :size :medium)]
-     (when-let [e @current-entity]
-       [:a.text-decoration-none.link-body-emphasis.fs-3.mx-3
-        {:data-bs-toggle "offcanvas"
-         :href "#entity-drawer"
-         :role :button
-         :aria-controls "entity-drawer"}
-        (:name e)])
-     [:ul.nav.col-12.col-lg-auto.me-lg-auto.mb-2.mb-lg-0.justify-content-center
-      [:li [:a.nav-link.px-2.link-secondary {:href "/about"} "About The App"]]]
-     [:div.dropdown.text-end.me-lg-3
-      [:a.d-block.link-body-emphasis.text-decoration-none.dropdown-toggle
-       {:data-bs-toggle :dropdown
-        :aria-expanded false}
-       [:span.me-2 (when-let [s @db-strategy]
-                     (name s))]
-       (icon :database :size :medium)]
-      (dropdown-ul (->> [:xtdb :datomic :sql :mongodb]
-                        (map (fn [id]
-                               {:id id
-                                :caption (id->caption id)
-                                :on-click #(reset! db-strategy id)}))))]
-     [:form.col-12.col-lg-auto.mb-2.mb-lg-0.me-lg-3
-      [:input.form-control {:type :text
-                            :placeholder "Search..."
-                            :aria-label "Search"}]]
-     [:div.dropdown.text-end
-      [:a.d-block.link-body-emphasis.text-decoration-none.dropdown-toggle
-       {:data-bs-toggle :dropdown
-        :aria-expanded false}
-       (icon :person-circle :size :medium)]
-      (dropdown-ul ["Sign In"])]]]])
+  (fn []
+    [:header.p-3.mb-3.border-bottom
+     [:div.container
+      [:div.d-flex.flex-wrap.align-items-center.justify-content-center.justify-content-lg-start
+       [:a.d-flex.align-items-center.mb-2.mb-lg-0.link-body-emphasis.text-decoration-none.me-2
+        {:href "/"}
+        (icon :cash-coin :size :medium)]
+       (when-let [e @current-entity]
+         [:a.text-decoration-none.link-body-emphasis.fs-3.mx-3
+          {:data-bs-toggle "offcanvas"
+           :href "#entity-drawer"
+           :role :button
+           :aria-controls "entity-drawer"}
+          (:name e)])
+       [:ul.nav.col-12.col-lg-auto.me-lg-auto.mb-2.mb-lg-0.justify-content-center
+        [:li [:a.nav-link.px-2.link-secondary {:href "/about"} "About The App"]]]
+       [:div.dropdown.text-end.me-lg-3
+        [:a.d-block.link-body-emphasis.text-decoration-none.dropdown-toggle
+         {:data-bs-toggle :dropdown
+          :aria-expanded false}
+         [:span.me-2 (when-let [s @db-strategy]
+                         (name s))]
+         (icon :database :size :medium)]
+        (dropdown-ul (->> [:xtdb :datomic :sql :mongodb]
+                          (map (fn [id]
+                                 {:id id
+                                  :caption (id->caption id)
+                                  :on-click #(reset! db-strategy id)}))))]
+       [:form.col-12.col-lg-auto.mb-2.mb-lg-0.me-lg-3
+        [:input.form-control {:type :text
+                              :placeholder "Search..."
+                              :aria-label "Search"}]]
+       [:div.dropdown.text-end
+        [:a.d-block.link-body-emphasis.text-decoration-none.dropdown-toggle
+         {:data-bs-toggle :dropdown
+          :aria-expanded false}
+         (icon :person-circle :size :medium)]
+        (dropdown-ul ["Sign In"])]]]]))
 
 (defn entity-drawer []
   (fn []
