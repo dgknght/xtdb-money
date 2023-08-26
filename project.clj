ecf4482552512d988087ff6b5f65e5bf66690e72
Doug Knight <dougbknight@gmail.com>
2023-08-26 09:59:45 -0500

    update ref to app lib

--- project.clj
+++ project.clj
@@ -52,7 +52,7 @@
                        [dev.weavejester/ragtime "0.9.3"]
                        [com.github.seancorfield/honeysql "2.4.1033"]
                        [congomongo "2.6.0" :exclusions [org.clojure/data.json]]
-                       [com.github.dgknght/app-lib "0.3.6-SNAPSHOT" :exclusions [args4j
+                       [com.github.dgknght/app-lib "0.3.7-SNAPSHOT" :exclusions [args4j
                                                                                  commons-logging
                                                                                  commons-io
                                                                                  ring/ring-core
