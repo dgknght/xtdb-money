1dd94dc49d7cf3a888d97406d139fe46248d4809
Doug Knight <dougbknight@gmail.com>
2023-12-01 17:15:35 -0600

    prepare transaction criteria better

--- src/xtdb_money/models/sql/transactions.clj
+++ src/xtdb_money/models/sql/transactions.clj
@@ -1,9 +1,12 @@
 (ns xtdb-money.models.sql.transactions
-  (:require [xtdb-money.sql.types :refer [->storable
-                                          <-storable]]
-            [xtdb-money.sql :as sql]
+  (:require [clojure.walk :refer [postwalk]]
+            [clojure.pprint :refer [pprint]]
             [dgknght.app-lib.core :refer [uuid
-                                          update-in-if]]))
+                                          update-in-if]]
+            [xtdb-money.util :refer [local-date?]]
+            [xtdb-money.sql.types :refer [->storable
+                                          <-storable]]
+            [xtdb-money.sql :as sql]))
 
 ; TODO: Dedupe this with what is also in mongodb
 (def ^:private attr
@@ -33,3 +36,24 @@
   (-> transaction
       (update-in-if [:correlation-id] uuid)
       (update-in [:transaction-date] <-storable)))
+
+(defn- extract-account-id
+  [{:keys [account-id] :as c}]
+  (if account-id
+    (with-meta [:and
+                (dissoc c :account-id)
+                [:or
+                 {:debit-account-id account-id}
+                 {:credit-account-id account-id}]]
+               (meta c))
+    c))
+
+(defn- prepare-criteria
+  [c]
+  (cond-> c
+    (map? c) extract-account-id
+    (local-date? c) ->storable))
+
+(defmethod sql/prepare-criteria :transaction
+  [criteria]
+  (postwalk prepare-criteria criteria))
