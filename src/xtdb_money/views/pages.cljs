ffccdaf47dc16521f22ee9499e7546598e12b82c
Doug Knight <dougbknight@gmail.com>
2023-12-02 10:16:36 -0600

    add sign-in page

--- src/xtdb_money/views/pages.cljs
+++ src/xtdb_money/views/pages.cljs
@@ -3,19 +3,22 @@
             [dgknght.app-lib.html :as html]
             [xtdb-money.state :refer [page]]))
 
+(defn- sign-in-options []
+  [:div.list-group {:style {:max-width "264px"}}
+   [:a.list-group-item.list-group-item-action.d-flex.align-items-center.p-0
+    {:href "/oauth/google"
+     :style {:background-color "#4285F4"
+             :font-weight :bold}}
+    [:div.bg-light.p-2.rounded
+     (html/google-g)]
+    [:div.text-center.w-100 "Sign In With Google"]]])
+
 (defn- welcome []
   (fn []
     [:div.container
      [:h1 "Welcome!"]
      [:p "There's lots of cool stuff coming soon."]
-     [:div.list-group {:style {:max-width "264px"}}
-      [:a.list-group-item.list-group-item-action.d-flex.align-items-center.p-0
-       {:href "/oauth/google"
-        :style {:background-color "#4285F4"
-                :font-weight :bold}}
-       [:div.bg-light.p-2.rounded
-        (html/google-g)]
-       [:div.text-center.w-100 "Sign In With Google"]]]]))
+     (sign-in-options)]))
 
 (defn- about []
   (fn []
@@ -28,6 +31,11 @@
    [:h1 "Not found"]
    [:p "The page you requested could not be found."]])
 
+(defn- sign-in []
+  [:div.container
+   [:h1 "Sign In"]
+   (sign-in-options)])
+
 (defroute welcome-path "/" []
   (reset! page welcome))
 
@@ -36,3 +44,6 @@
 
 (defroute not-found-path "/#not-found" []
   (reset! page not-found))
+
+(defroute sign-in-path "/sign-in" []
+  (reset! page sign-in))
